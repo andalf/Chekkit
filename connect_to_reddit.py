0fcd784146b4696e742a9950227036a455cad93c
andalf <andy@gorocketfuel.com>
2015-12-05 12:45:52 -0600

    Broken but a start to making a user input function.

--- connect_to_reddit.py
+++ connect_to_reddit.py
@@ -5,3 +5,13 @@ import praw
 def connect(user_agent):
     # connect to Reddit as script specified user agent
     return praw.Reddit(user_agent=user_agent)
+
+
+def which_subreddit():
+    # lets user choose subreddit
+    sub_reddit = input("What Subreddit would you like to use?")
+
+    sub_reddit = sub_reddit.lower()
+
+    return sub_reddit
+
