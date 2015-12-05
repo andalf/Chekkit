77b35c975bcefd5c6ec19bd09a52171493055239
andalf <andy@gorocketfuel.com>
2015-12-05 11:45:23 -0600

    Updated to pprint++

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import praw
 import sys
+import pp
 
-print (sys.version)
+pp(sys.version)
 
 r = praw.Reddit(user_agent='my_cool_application')
 submissions = r.get_subreddit('opensource').get_hot(limit=5)
-print ([str(x) for x in submissions])
+pp([str(x) for x in submissions])
