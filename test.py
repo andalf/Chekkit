1ec86f66c77d5f44a617ed30b1e8f97b715be702
andalf <andy@gorocketfuel.com>
2015-12-05 11:18:49 -0600

    Added a version check into the script.

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import praw
+import sys
+
+print sys.version
+
 r = praw.Reddit(user_agent='my_cool_application')
 submissions = r.get_subreddit('opensource').get_hot(limit=5)
 print [str(x) for x in submissions]
