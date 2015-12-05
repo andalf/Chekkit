a66fab8519721c5042fdbb774ca7d2816a7e0c6d
wellgregory15 <tylerleedixon@gmail.com>
2015-12-05 12:12:47 -0600

    merge with dev fixing conflicts

--- test.py
+++ test.py
@@ -12,8 +12,11 @@ pp(sys.version)
 # connect to Reddit as Chekkit
 r = praw.Reddit(user_agent='chekkit')
 
+#lets user choose subreddit
+subReddit = input("What Subreddit would you like to use?")
+
 # get 5 most recent from the subReddit 'opensource'
-submissions = r.get_subreddit('opensource').get_hot(limit=5)
+submissions = r.get_subreddit(subReddit).get_hot(limit=5)
 
 # print out the output
 pp([str(x) for x in submissions])
