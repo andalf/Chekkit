e2cfa1f55d59f59ad4434a2e49d3a4942ee4b2c0
wellgregory15 <tylerleedixon@gmail.com>
2015-12-10 20:53:23 -0600

    changed  the subReddit variable to get what it is from the "which_subreddit" function made function in connect_to_reddit to ask how many to show and then made that as the amount in pull recent for the limit= part

--- connect_to_reddit.py
+++ connect_to_reddit.py
@@ -1,7 +1,6 @@
 # pip install praw
 import praw
 
-
 def connect(user_agent):
     # connect to Reddit as script specified user agent
     return praw.Reddit(user_agent=user_agent)
@@ -10,9 +9,7 @@ def connect(user_agent):
 def which_subreddit():
     # lets user choose subreddit
     sub_reddit = input("What Subreddit would you like to use?")
-
     sub_reddit = sub_reddit.lower()
-
     return sub_reddit
 
 def change_limitnumber():
@@ -21,4 +18,10 @@ def change_limitnumber():
     limit_number = int(limit_number)
 
     return limit_number
+
+
+def account_login():
+    username = input("What is your user name?")
+    password = input("What is your reddit password?")
+    r.login(username, password)
 
